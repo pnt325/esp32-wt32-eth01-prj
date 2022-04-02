86eed1353d85f7521fb9d795e77f522c6ea6a65e
pnt325 <phat.nt@hotmail.com>
2022-04-02 23:05:00 +0700

    Add wifi

--- firmware/main/main.c
+++ firmware/main/main.c
@@ -16,10 +16,25 @@
 #include "esp_log.h"
 #include "driver/gpio.h"
 #include "sdkconfig.h"
+#include "nvs_flash.h"
 #include "eth.h"
+#include "wifi.h"
 
 void app_main(void)
 {
+    //Initialize NVS
+    esp_err_t ret = nvs_flash_init();
+    if (ret == ESP_ERR_NVS_NO_FREE_PAGES || ret == ESP_ERR_NVS_NEW_VERSION_FOUND) {
+      ESP_ERROR_CHECK(nvs_flash_erase());
+      ret = nvs_flash_init();
+    }
+    ESP_ERROR_CHECK(ret);
+
+#if 0
     ETH_init();
     ETH_start();
+#else
+    WIFI_init();
+    WIFI_start();
+#endif
 }
---
+++ firmware/main/wifi.h
@@ -0,0 +1,14 @@
+/*
+ * wifi.h
+ *
+ *  Created on: Apr 02, 2022
+ *      Author: Phat.N
+ */
+
+#ifndef _WIFI_H_
+#define _WIFI_H_
+
+void WIFI_start(void);
+void WIFI_stop(void);
+
+#endif /*_WIFI_H_*/
